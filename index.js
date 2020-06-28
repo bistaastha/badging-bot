81b1b9016acb32b2e7a1e5e06bfb3d1be3fdaaf8
Aastha Bist <abist119@gmail.com>
2020-06-28 18:23:02 +0530

    Add pull_request.assignment event to index.js

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 module.exports = (app) => {
-  app.on("pull_request.opened", postChecklist);
+  app.on("pull_request.opened", postWelcome);
+  app.on("pull_request.assigned", postChecklist);
 
   async function postChecklist(context) {
 
-    const res = await context.github.repos.getContents(
+    const heading = `# Checklist for @${context.payload.assignee.login}`;
+
+    const reviewerWelcome = await context.github.repos.getContents(
+      context.repo({path: '.github/reviewer-welcome.md'})
+    );
+
+    const checklist = await context.github.repos.getContents(
       context.repo({path: '.github/checklist.md'})
     );
-    const checklist = Buffer.from(res.data.content, 'base64').toString();
+
+    const reviewerMessage = heading + "\n" + Buffer.from(reviewerWelcome.data.content, 'base64').toString() +
+                            Buffer.from(checklist.data.content, 'base64').toString();
 
     context.github.issues.createComment(
-      context.issue({ body: checklist })
+      context.issue({ body: reviewerMessage })
     );
 
+  }
+
+  async function postWelcome(context) {
+    const applicantWelcome = await context.github.repos.getContents(
+      context.repo({path: '.github/applicant-welcome.md'})
+    );
+
+    const applicantMessage = Buffer.from(applicantWelcome.data.content, 'base64').toString();
+
+    context.github.issues.createComment(
+      context.issue({ body: applicantMessage })
+    );
   }
 };
