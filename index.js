feb3af7be3e32270ff981c4b1bdb4050b30a4431
Aastha Bist <abist119@gmail.com>
2020-08-12 19:18:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,26 +5,4 @@ module.exports = (app) => {
   app.on("pull_request.opened", postWelcome);
   app.on("pull_request.assigned", postChecklist);
 
-  async function postChecklist(context) {
-
-    const heading = `# Checklist for @${context.payload.assignee.login}`;
-
-    const reviewerWelcome = await context.github.repos.getContents(
-      context.repo({path: '.github/reviewer-welcome.md'})
-    );
-
-    const checklist = await context.github.repos.getContents(
-      context.repo({path: '.github/checklist.md'})
-    );
-
-    const reviewerMessage = heading + "\n" + Buffer.from(reviewerWelcome.data.content, 'base64').toString() +
-                            Buffer.from(checklist.data.content, 'base64').toString();
-
-    context.github.issues.createComment(
-      context.issue({ body: reviewerMessage })
-    );
-
-  }
-
-  
 };
