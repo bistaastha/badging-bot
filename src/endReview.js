8415da0023b4c053fea9c14f88a53580e419937d
Glitch (bistaastha-list-bot-test-2) <none>
2020-08-16 14:39:44 +0000

    Edit endReview and commandResponse

--- src/endReview.js
+++ src/endReview.js
@@ -1,8 +1,17 @@
+const calculateBadge = require('./calculateBadge');
+
 async function endReview(context) {
 
-    context.github.issues.removeLabels(context.issue({name: ["review-begin"]}))
+    let reviewDetails = await calculateBadge(context);
+    context.github.issues.removeLabels(context.issue({name: ["review-begin"]}));
     context.github.issues.addLabels(context.issue({labels: ["review-end"]}));
-    return context.github.issues.update(context.issue({state: 'closed'}))
+    context.github.issues.update(context.issue({state: "closed"}));
+
+    const message = "\n**Markdown Badge Link: **\n```\n" + reviewDetails[0] + "\n```"
+    + "\n**HTML Badge Link: **\n```\n" + reviewDetails[1] + "\n```";
+  
+    return context.github.issues.createComment(
+        context.issue({ body: reviewDetails[0] + message}));
 }
 
 module.exports = endReview;
